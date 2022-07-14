90725a622dcd66f7223fd9a0eb7ff8bd03c96039
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2022-07-14 11:06:31 -0500

    fix

--- src/components/ShiftStatistic/ClosedShiftStatistic.js
+++ src/components/ShiftStatistic/ClosedShiftStatistic.js
@@ -8,45 +8,7 @@ const ClosedShiftStatistic = (props) => {
     <List {...props} aside={<FilterSideBar />}>
       <Datagrid>
         <TextField source="employeeId" label={<Label label="Employee Id" />} />
-        <TextField
-          source="printedPhotos"
-          label={<Label label="Printed Photos" />}
-        />
-        <TextField
-          source="printedPostcards"
-          label={<Label label="Printed Postcards" />}
-        />
-        <TextField source="soldByCard" label={<Label label="Sold by card" />} />
-        <TextField source="soldByCash" label={<Label label="Sold by cash" />} />
-        <TextField source="soldItems" label={<Label label="Sold items" />} />
-        <TextField
-          source="soldItemsSquare"
-          label={<Label label="Sold items square" />}
-        />
-        <TextField
-          source="takenPhotos"
-          label={<Label label="Taken photos" />}
-        />
-        <TextField
-          source="totalMinutes"
-          label={<Label label="Total minutes" />}
-        />
-        <TextField
-          source="totalSoldInUsd"
-          label={<Label label="Total sold in usd" />}
-        />
-        <TextField
-          source="totalSoldSquareInUsd"
-          label={<Label label="Total sold square in usd" />}
-        />
-        <TextField
-          source="totalTables"
-          label={<Label label="Total tables" />}
-        />
-        <TextField
-          source="uploadedFiles"
-          label={<Label label="Uploaded Files" />}
-        />
+        <TextField source="placeId" label={<Label label="Place Id" />} />
         <DateField
           source="clockInTime"
           label={<Label label="Clock In Time" />}
@@ -55,10 +17,50 @@ const ClosedShiftStatistic = (props) => {
         />
         <DateField
           source="clockOutTime"
-          label={<Label label="Clock Out Time" />}
+          label={<Label label="Clock In Time" />}
           showTime
           locales="en-US"
         />
+        <TextField
+          source="totalMinutes"
+          label={<Label label="Total minutes" />}
+        />
+        <TextField
+          source="totalTables"
+          label={<Label label="Total tables" />}
+        />
+
+        <TextField
+          source="takenPhotos"
+          label={<Label label="Taken photos" />}
+        />
+        <TextField
+          source="uploadedFiles"
+          label={<Label label="Uploaded Files" />}
+        />
+        <TextField
+          source="printedPhotos"
+          label={<Label label="Printed Photos" />}
+        />
+        <TextField
+          source="printedPostcards"
+          label={<Label label="Printed Postcards" />}
+        />
+        <TextField source="soldItems" label={<Label label="Sold items" />} />
+        <TextField
+          source="totalSoldInUsd"
+          label={<Label label="Total sold in usd" />}
+        />
+        <TextField
+          source="soldItemsSquare"
+          label={<Label label="Sold items square" />}
+        />
+        <TextField
+          source="totalSoldSquareInUsd"
+          label={<Label label="Total sold square in usd" />}
+        />
+        <TextField source="soldByCard" label={<Label label="Sold by card" />} />
+        <TextField source="soldByCash" label={<Label label="Sold by cash" />} />
       </Datagrid>
     </List>
   );
--- src/components/ShiftStatistic/OpenShiftStatistic.js
+++ src/components/ShiftStatistic/OpenShiftStatistic.js
@@ -8,6 +8,30 @@ const OpenShiftStatistic = (props) => {
     <List {...props} aside={<FilterSideBar />}>
       <Datagrid>
         <TextField source="employeeId" label={<Label label="Employee Id" />} />
+        <TextField source="placeId" label={<Label label="Place Id" />} />
+        <DateField
+          source="clockInTime"
+          label={<Label label="Clock In Time" />}
+          showTime
+          locales="en-US"
+        />
+        <TextField
+          source="totalMinutes"
+          label={<Label label="Total minutes" />}
+        />
+        <TextField
+          source="totalTables"
+          label={<Label label="Total tables" />}
+        />
+
+        <TextField
+          source="takenPhotos"
+          label={<Label label="Taken photos" />}
+        />
+        <TextField
+          source="uploadedFiles"
+          label={<Label label="Uploaded Files" />}
+        />
         <TextField
           source="printedPhotos"
           label={<Label label="Printed Photos" />}
@@ -16,43 +40,21 @@ const OpenShiftStatistic = (props) => {
           source="printedPostcards"
           label={<Label label="Printed Postcards" />}
         />
-        <TextField source="soldByCard" label={<Label label="Sold by card" />} />
-        <TextField source="soldByCash" label={<Label label="Sold by cash" />} />
         <TextField source="soldItems" label={<Label label="Sold items" />} />
-        <TextField
-          source="soldItemsSquare"
-          label={<Label label="Sold items square" />}
-        />
-        <TextField
-          source="takenPhotos"
-          label={<Label label="Taken photos" />}
-        />
-        <TextField
-          source="totalMinutes"
-          label={<Label label="Total minutes" />}
-        />
         <TextField
           source="totalSoldInUsd"
           label={<Label label="Total sold in usd" />}
         />
         <TextField
+          source="soldItemsSquare"
+          label={<Label label="Sold items square" />}
+        />
+        <TextField
           source="totalSoldSquareInUsd"
           label={<Label label="Total sold square in usd" />}
         />
-        <TextField
-          source="totalTables"
-          label={<Label label="Total tables" />}
-        />
-        <TextField
-          source="uploadedFiles"
-          label={<Label label="Uploaded Files" />}
-        />
-        <DateField
-          source="clockInTime"
-          label={<Label label="Clock In Time" />}
-          showTime
-          locales="en-US"
-        />
+        <TextField source="soldByCard" label={<Label label="Sold by card" />} />
+        <TextField source="soldByCash" label={<Label label="Sold by cash" />} />
       </Datagrid>
     </List>
   );
